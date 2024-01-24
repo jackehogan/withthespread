966d16f20c41a7ac50a72a0ec21bae5e9804ca3b
mummysboy <isaachirsch@gmail.com>
2024-01-23 16:39:46 -0800

    build
    build

--- client/server/index.js
+++ client/server/index.js
@@ -4,7 +4,7 @@ const { MongoClient } = require("mongodb");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000; // Fallback to 3001 if process.env.PORT is not defined
+const port = process.env.PORT || 3001; // Fallback to 3001 if process.env.PORT is not defined
 
 const userName = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
